69946f0c3741604f07ba342e3daaf62e1e1bf222
Junya Oiwa <0920oiwa@gmail.com>
2020-05-30 21:39:00 +0900

    [add]画像ダウンロード部分を実装

--- index.js
+++ index.js
@@ -8,7 +8,6 @@ const tools = require('./tools');
 client.on('ready', () => {
   console.log('Discord-Image-Downloader by 920OJ');
   console.log(`${client.user.username}としてログインしました。`);
-  tools.pixivDownload('https://www.pixiv.net/artworks/76958795');
 })
 
 client.login(process.env.DSTOKEN);
--- tools.js
+++ tools.js
@@ -15,16 +15,16 @@ exports.pixivDownload = async (e) => {
   const illust_id = url.pathname.replace('/artworks/', '');
   const options = {
     headers: {
-      'refferer': `https://www.pixiv.net/artworks/${illust_id}`,
+      'referer': `https://www.pixiv.net/artworks/${illust_id}`,
       'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
     }
-  }
+  };
   const res = await axios.get(`https://www.pixiv.net/ajax/illust/${illust_id}?lang=ja`, options);
   const illust_info = res.data;
   if (illust_info.body.pageCount > 1) {
     const res = await axios.get(`https://www.pixiv.net/ajax/illust/${illust_id}/pages`, options);
     const page_info = res.data;
-    const url_list = []
+    const url_list = [];
     page_info.body.forEach(e => {
       url_list.push(e.urls.original);
     })
@@ -32,10 +32,31 @@ exports.pixivDownload = async (e) => {
   }
   else {
     const illust_original_url = illust_info.body.urls.original;
-    console.log(illust_original_url);
+    const file_name =
+      console.log(illust_original_url);
+    await this.imageDownload(illust_original_url, `https://www.pixiv.net/artworks/${illust_id}`);
   }
 }
 
-exports.imageDownload = (e) => {
+exports.imageDownload = (url, referer, file_name) => {
+  const fs = require('fs');
+  const axios = require('axios');
 
+  if (!fs.existsSync('./dl/')) {
+    fs.mkdirSync('./dl/');
+  };
+  const options = {
+    headers: {
+      'referer': referer,
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    },
+    responseType: 'arraybuffer'
+  };
+
+  return new Promise(async (resolve, reject) => {
+    await axios.get(url, options).then(res => {
+      fs.writeFileSync('test.png', res.data, 'binary');
+      resolve();
+    });
+  })
 }
