9af9f533f35e0ac2e6416160f8e7fd54216d4eb5
Junya Oiwa <0920oiwa@gmail.com>
2020-05-30 20:06:45 +0900

    [add]Pixivダウンロード部分・画像URL抽出を実装

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const tools = require('./tools');
 client.on('ready', () => {
   console.log('Discord-Image-Downloader by 920OJ');
   console.log(`${client.user.username}としてログインしました。`);
+  tools.pixivDownload('https://www.pixiv.net/artworks/76958795');
 })
 
 client.login(process.env.DSTOKEN);
--- tools.js
+++ tools.js
@@ -8,3 +8,34 @@ exports.getUrlList = (content) => {
     return list;
   }
 }
+
+exports.pixivDownload = async (e) => {
+  const axios = require('axios');
+  const url = new URL(e);
+  const illust_id = url.pathname.replace('/artworks/', '');
+  const options = {
+    headers: {
+      'refferer': `https://www.pixiv.net/artworks/${illust_id}`,
+      'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
+    }
+  }
+  const res = await axios.get(`https://www.pixiv.net/ajax/illust/${illust_id}?lang=ja`, options);
+  const illust_info = res.data;
+  if (illust_info.body.pageCount > 1) {
+    const res = await axios.get(`https://www.pixiv.net/ajax/illust/${illust_id}/pages`, options);
+    const page_info = res.data;
+    const url_list = []
+    page_info.body.forEach(e => {
+      url_list.push(e.urls.original);
+    })
+    console.log(url_list);
+  }
+  else {
+    const illust_original_url = illust_info.body.urls.original;
+    console.log(illust_original_url);
+  }
+}
+
+exports.imageDownload = (e) => {
+
+}
