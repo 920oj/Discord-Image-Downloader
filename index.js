8bdcf1ab2cdab47e2d1102174cd34436b909c361
Junya Oiwa <0920oiwa@gmail.com>
2020-05-31 03:37:35 +0900

    [add]Pixivダウンロード処理を別ファイルに切り分け

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ const pixiv = require('./lib/pixiv');
 const channel = process.env.CH.split(' ');
 
 client.on('ready', () => {
-  if (!fs.existsSync('./dl/')) {
-    fs.mkdirSync('./dl/');
-  };
+  if (!fs.existsSync('./dl/')) fs.mkdirSync('./dl/');
+  channel.forEach((e) => { if (!fs.existsSync(`./dl/${e}/`)) fs.mkdirSync(`./dl/${e}/`); });
+
   console.log('Discord-Image-Downloader by 920OJ');
   console.log(`${client.user.username}としてログインしました。`);
 })
@@ -26,40 +26,10 @@ client.on('message', async msg => {
           if (e.indexOf('twitter.com') != -1) { // リンクがTwitterだった時の処理 
 
           }
-          else if (e.indexOf('pixiv.net') != -1) { // リンクがPixivだった時の処理
-            const pixiv_dl_info = await pixiv.pixivImgParser(e);
-            const illust_id = pixiv_dl_info.id;
-
-            if (pixiv_dl_info.urls.length > 1) {
-              if (!fs.existsSync(`./dl/${illust_id}/`)) {
-                fs.mkdirSync(`./dl/${illust_id}/`);
-              };
-              pixiv_dl_info.urls.forEach(async url => {
-                const pixiv_options = {
-                  headers: {
-                    'referer': `https://www.pixiv.net/artworks/${illust_id}`,
-                    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-                  },
-                  responseType: 'arraybuffer'
-                };
-                const file_name = new URL(url).pathname.split('/').slice(-1)[0];
-                await tools.downloadImage(url, pixiv_options, `./dl/${illust_id}/${file_name}`);
-              });
-            }
-            else if (pixiv_dl_info.urls.length === 1) {
-              const pixiv_options = {
-                headers: {
-                  'referer': `https://www.pixiv.net/artworks/${illust_id}`,
-                  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-                },
-                responseType: 'arraybuffer'
-              };
-              const file_name = new URL(pixiv_dl_info.urls[0]).pathname.split('/').slice(-1)[0];
-              await tools.downloadImage(pixiv_dl_info.urls[0], pixiv_options, `./dl/${file_name}`);
-            }
-            else {
-
-            }
+          else if (e.indexOf('pixiv.net') != -1 && e.indexOf('artworks') != -1) { // リンクがPixivだった時の処理
+            const illust_data = await pixiv.pixivImgParser(e);
+            console.log(illust_data);
+            await pixiv.pixivDownloader(illust_data, msg.channel.id);
           }
         });
       }
