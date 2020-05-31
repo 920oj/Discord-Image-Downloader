d055341ab8962666c6fd6059362268b676f8007c
Junya Oiwa <0920oiwa@gmail.com>
2020-05-31 16:42:08 +0900

    [add]Twitterダウンロードを実装

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 require('dotenv').config();
 
 const fs = require('fs');
+
 const discord = require('discord.js');
 const client = new discord.Client();
 
+const tw = require('twitter');
+const tw_client = new tw({
+  consumer_key: process.env.TW_CK,
+  consumer_secret: process.env.TW_CS,
+  access_token_key: process.env.TW_TK,
+  access_token_secret: process.env.TW_TS
+})
+
 const tools = require('./lib/tools');
+const twitter = require('./lib/twitter');
 const pixiv = require('./lib/pixiv');
 
 const channel = process.env.CH.split(' ');
@@ -24,7 +34,20 @@ client.on('message', async msg => {
       if (url_list) {
         for (let e of url_list) {
           if (e.indexOf('twitter.com') != -1) { // リンクがTwitterだった時の処理 
-
+            const tweet_id = twitter.parseTweetId(e);
+            const tweet_info = await twitter.getTweetInfo(tweet_id, tw_client);
+            if (tweet_info.extended_entities) {
+              if (tweet_info.extended_entities.media[0].type === 'photo') {
+                const tweet_image_data = twitter.parseTweetData(tweet_info);
+                await twitter.twitterDwonloader(tweet_image_data, tweet_id, msg.channel.id)
+                  .then(() => {
+                    msg.react('👌');
+                  })
+                  .catch(() => {
+                    msg.react('❌');
+                  })
+              }
+            }
           }
           else if (e.indexOf('pixiv.net') != -1 && e.indexOf('artworks') != -1) { // リンクがPixivだった時の処理
             await pixiv.pixivImgParser(e)
